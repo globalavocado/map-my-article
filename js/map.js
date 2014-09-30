77f47176a7df2ba4f70e202e2f2d2c0ae059f951
globalavocado <globalavocado@gmail.com>
2014-09-30 15:47:05 +0100

    increased to 20 search results, made map browser responsive, changed base map to Mapbox

--- js/map.js
+++ js/map.js
@@ -1,11 +1,21 @@
 	// create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map').setView([9.644, 39.683], 5);
+	// Ethiopia
+var map = L.map('map').setView([9.644, 39.683], 6);
 
-// add an OpenStreetMap tile layer
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	//Indonesia
+// var map = L.map('map').setView([-2.53, 120.43], 4);
+
+// add an MapBox tile layer
+
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/{your-MapboxID-goes-here}/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
 }).addTo(map);
 
 // add a marker in the given location, attach some popup content to it and open the popup
-L.marker([9.644, 39.683]).addTo(map)
+// Ethiopia marker
+// L.marker([9.644, 39.683]).addTo(map)
+
+// Indonesia marker
+// L.marker([-6.124, 106.48]).addTo(map)
 //     .bindPopup($('#country').val())
