b3dc65fc62290f4137a39fd7524b47264b250177
globalavocado <globalavocado@gmail.com>
2014-10-05 12:58:03 +0100

    implemented dropdown search, edited README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Guardian API and OpenStreetMap Nominatim API feed mashup, extracting the relevant country from an article and displaying a map of the country next to it.
+<h1>Map My Article</h1>
+
+Guardian API and Leaflet API feed mashup, searching from a list of 2014 Developing Countries and displaying a map of that country next to it.
+
+<h2>Instructions</h2>
+
+In order to get the map to work, you need to sign up for a MapBox ID here and insert it into the code. Alternatively you can comment out Mapbox and enable the OpenStreetMap API.
--- js/map.js
+++ js/map.js
@@ -6,11 +6,15 @@ var map = L.map('map').setView([9.644, 39.683], 6);
 // var map = L.map('map').setView([-2.53, 120.43], 4);
 
 // add an MapBox tile layer
-
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/{your-MapboxID-goes-here}/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
 }).addTo(map);
+
+// comment this out if you want to use OpenStreetMap instead of signing up for MapBox
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
 
 // add a marker in the given location, attach some popup content to it and open the popup
 // Ethiopia marker
