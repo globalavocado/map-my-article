4a0124648fbdff312075cd613825713cf68c1ce9
globalavocado <globalavocado@users.noreply.github.com>
2015-01-23 19:42:18 +0000

    amended README and added gitignore

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This web app does a fuzzy search on the <a href="http://www.isi-web.org/component/content/article/5-root/root/81-developing">ISI list of developing countries for 2014</a>. It uses the Guardian API to return a list of news article links and then the Leaflet API displays a MapBox base map of that country next to the list.
 
+The map is meant to zoom to the country that is searched for on the left hand side, every time a new search is performed. I plan to implement this with OpenLayers 3.
+
 <h2>Technologies used</h2>
 - HTML5
 - CSS3
--- js/map.js
+++ js/map.js
@@ -1,5 +1,4 @@
-	// create a map in the "map" div, set the view to a given place and zoom
-	// Ethiopia
+// Ethiopia
 // var map = L.map('map').setView([9.644, 39.683], 6);
 
 // Sudan and South Sudan
@@ -8,14 +7,12 @@
 	//Indonesia
 // var map = L.map('map').setView([-2.53, 120.43], 4);
 
-// create a map
 map = new L.Map('map');
 
-// add a MapBox tile layer
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/{your-MapboxID-goes-here}/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
-  })
+    })
   .addTo(map);
 
 // comment this out if you want to use OpenStreetMap instead of signing up for MapBox
@@ -23,12 +20,11 @@ L.tileLayer('http://{s}.tiles.mapbox.com/v3/{your-MapboxID-goes-here}/{z}/{x}/{y
 //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 // }).addTo(map);
 
-// set the map's starting view at Haiti
 map.setView( new L.LatLng(19.5, -72), 7 );
 
-// include GeoJSON of Haiti
 L.geoJson( haiti, {
     style: function (feature) {
         return { opacity: 0.1, fillOpacity: 0.5, fillColor: "#FF7F50" };
     }
-}).addTo(map);
+  })
+  .addTo(map);
