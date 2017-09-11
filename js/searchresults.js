f0f3c63d30bf0afe0d88935a4432f0b0620d8517
Kevin <globalavocado@users.noreply.github.com>
2017-09-11 15:11:48 +0100

    fixed placeholder visibility & styled search button

--- js/searchresults.js
+++ js/searchresults.js
@@ -1,6 +1,6 @@
        var config = {
         '.chosen-select'           : {},
-        '.chosen-select-deselect'  : {allow_single_deselect:true},
+        '.chosen-select-deselect'  : {allow_single_deselect:false},
         '.chosen-select-no-single' : {disable_search_threshold:10},
         '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
         '.chosen-select-width'     : {width:"95%"}
